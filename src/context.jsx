a9c57bc7f1ed1b035195c1a9ebb93d52db11708a
Konrad <okonskikonrad94@gmail.com>
2023-05-29 23:42:11 +0200

    dev

---
+++ src/context.jsx
@@ -0,0 +1,31 @@
+import {useContext, useReducer, useEffect, createContext} from "react";
+import reducer from "./reducer.js";
+import {CLEAR_CART, REMOVE, INCREASE, DECREASE, LOADING, DISPLAY_ITEMS} from "./actions.js";
+import cartItems from "./data.jsx";
+
+
+const AppContext = createContext();
+
+
+const initialState = {
+    loading:false,
+    cart: new Map(cartItems.map((item)=> [item.id, item])),
+};
+
+
+export const AppProvider = ({children}) => {
+    const [state, dispatch] = useReducer(reducer, initialState)
+
+    const clearCart = () => {
+        dispatch({type:CLEAR_CART});
+    }
+
+    return (
+    <AppContext.Provider value={{...state, clearCart}}>
+        {children}
+    </AppContext.Provider>
+    );
+};
+export const useGlobalContext = () => {
+    return useContext(AppContext);
+}
---
+++ src/reducer.js
@@ -0,0 +1,10 @@
+import {CLEAR_CART, REMOVE, INCREASE, DECREASE, LOADING, DISPLAY_ITEMS} from "./actions.js";
+
+const reducer = (state, action) => {
+    if (action.type === CLEAR_CART){
+        return {...state, cart: new Map()};
+    }
+    throw new Error(`no matching action type : ${action.type}`);
+};
+
+export default reducer;
