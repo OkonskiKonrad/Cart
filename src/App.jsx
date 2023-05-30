3490eebd8eba0676fd8a022355b595e72968cdfe
Konrad <okonskikonrad94@gmail.com>
2023-05-30 15:39:57 +0200

    dev

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 // components
 import Navbar from './Navbar';
 import CartContainer from './CartContainer';
+import {useGlobalContext} from "./context.jsx";
 
 function App() {
-  return (
-    <main>
-      <Navbar />
-      <CartContainer />
-    </main>
-  );
+    const {loading} = useGlobalContext();
+    if (loading) {
+        return (
+            <main>
+                <div className='loading' style={{marginTop: '6rem'}}></div>
+            </main>
+        )
+    }
+    return (
+        <main>
+            <Navbar/>
+            <CartContainer/>
+        </main>
+    );
 }
 
 export default App;
--- src/context.jsx
+++ src/context.jsx
@@ -2,19 +2,23 @@ import {useContext, useReducer, useEffect, createContext} from "react";
 import reducer from "./reducer.js";
 import {CLEAR_CART, REMOVE, INCREASE, DECREASE, LOADING, DISPLAY_ITEMS} from "./actions.js";
 import cartItems from "./data.jsx";
+import {getTotals} from "./utils.js";
 
+const url = 'https://www.course-api.com/react-useReducer-cart-project';
 
 const AppContext = createContext();
 
 
 const initialState = {
     loading: false,
-    cart: new Map(cartItems.map((item) => [item.id, item])),
+    cart: new Map(),
 };
 
 
 export const AppProvider = ({children}) => {
     const [state, dispatch] = useReducer(reducer, initialState)
+
+    const {totalAmount, totalCost} = getTotals(state.cart)
 
     const clearCart = () => {
         dispatch({type: CLEAR_CART});
@@ -28,9 +32,19 @@ export const AppProvider = ({children}) => {
     const decrease = (id) => {
         dispatch({type: DECREASE, payload: {id}})
     }
+    const fetchData = async () => {
+        dispatch({type: LOADING})
+        const response = await fetch(url);
+        const cart = await response.json();
+        dispatch({type: DISPLAY_ITEMS, payload: {cart}})
+
+    }
+    useEffect(() => {
+        fetchData()
+    }, [])
 
     return (
-        <AppContext.Provider value={{...state, clearCart, remove, increase, decrease}}>
+        <AppContext.Provider value={{...state, clearCart, remove, increase, decrease, totalCost, totalAmount}}>
             {children}
         </AppContext.Provider>
     );
